da0c10cadaba5e0dd46bbe02d7607369871b8593
Mateusz <mateuszd2411@gmail.com>
2020-04-24 19:39:22 +0200

    I added JumpPlayer class

--- core/src/com/mygdx/jumpGame/JumpGame.java
+++ core/src/com/mygdx/jumpGame/JumpGame.java
@@ -1,19 +1,26 @@
 package com.mygdx.jumpGame;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class JumpGame extends ApplicationAdapter {
 
-
+	SpriteBatch batch;
 	
 	@Override
 	public void create () {
-
+		batch = new SpriteBatch();
 	}
 
 	@Override
 	public void render () {
+		Gdx.gl.glClearColor(1,1,1,1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+		batch.begin();
+		batch.end();
 	}
 
     private void update() {
--- desktop/src/com/mygdx/jumpGame/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/jumpGame/desktop/DesktopLauncher.java
@@ -7,8 +7,8 @@ import com.mygdx.jumpGame.JumpGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = 800;
-		config.height = 480;
+		config.width = 480;
+		config.height = 800;
 		config.resizable = false;
 		new LwjglApplication(new JumpGame(), config);
 	}
