e0fcc88d992086c6324685bff97190aed120285f
Mateusz <mateuszd2411@gmail.com>
2020-04-24 18:14:10 +0200

    Start new Jump Game project

---
+++ core/src/com/mygdx/jumpGame/JumpGame.java
@@ -0,0 +1,32 @@
+package com.mygdx.jumpGame;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class JumpGame extends ApplicationAdapter {
+
+
+	
+	@Override
+	public void create () {
+
+
+	}
+
+	@Override
+	public void render () {
+
+
+
+	}
+
+    private void update() {
+
+
+    }
+
+    //clear member after close app
+	@Override
+	public void dispose() {
+
+	}
+}
---
+++ desktop/src/com/mygdx/jumpGame/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.jumpGame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.jumpGame.JumpGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 480;
+		config.resizable = false;
+		new LwjglApplication(new JumpGame(), config);
+	}
+}
